161aef9080118466ea98d0920b8cb4b5eb123528
james <25brimwood@gmail.com>
2026-07-27 11:08:27 -0400

    add admin to public

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,16 +1,5 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
-export interface HistoriesHistories extends Struct.ComponentSchema {
-  collectionName: 'components_histories_histories';
-  info: {
-    displayName: 'histories';
-  };
-  attributes: {
-    image: Schema.Attribute.Media<'images'>;
-    text: Schema.Attribute.Blocks;
-  };
-}
-
 export interface ImageTextImageText extends Struct.ComponentSchema {
   collectionName: 'components_image_text_image_texts';
   info: {
@@ -22,22 +11,10 @@ export interface ImageTextImageText extends Struct.ComponentSchema {
   };
 }
 
-export interface TimespanTimespans extends Struct.ComponentSchema {
-  collectionName: 'components_timespan_timespans';
-  info: {
-    displayName: 'timespans';
-  };
-  attributes: {
-    text: Schema.Attribute.Blocks;
-  };
-}
-
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'histories.histories': HistoriesHistories;
       'image-text.image-text': ImageTextImageText;
-      'timespan.timespans': TimespanTimespans;
     }
   }
 }
